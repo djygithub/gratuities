3a4549d1bfbd45b79be8e3a4ec13bbcd9123e986
djygithub <david@davidjyoung.com>
2017-03-07 09:39:46 -0800

    commit it

---
+++ tippy/SettingsViewController.swift
@@ -0,0 +1,97 @@
+//
+//  SettingsViewController.swift
+//  tippy
+//
+//  Created by David on 3/6/17.
+//  Copyright © 2017 David. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    //@IBOutlet weak var setdeftip: UISegmentedControl!
+    @IBOutlet weak var tip18: UIButton!
+    @IBOutlet weak var tipper: UIButton!
+    @IBOutlet weak var tip20: UIButton!
+    
+            override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+            
+            let valueToSave = 0
+            
+            //UserDefaults.standard.integer(forKey: <#T##String#>)
+            
+            UserDefaults.standard.set(valueToSave, forKey: "segid")
+            UserDefaults.standard.synchronize()
+            //_ = UserDefaults.standard.integer(forKey: "segid")
+ 
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func tip20set(_ sender: AnyObject) {
+        let segvalueToSave = 1
+        //let segvalueToSave = seg[setvalueToSave.selectedSegmentIndex]
+        
+        
+        
+        
+        
+        
+        //UserDefaults.standard.integer(forKey: <#T##String#>)
+        
+        UserDefaults.standard.set(segvalueToSave, forKey: "segid")
+        UserDefaults.standard.synchronize()
+
+    }
+    
+    @IBAction func tip18set(_ sender: AnyObject) {
+        let segvalueToSave = 0
+        
+        //let segvalueToSave = seg[setvalueToSave.selectedSegmentIndex]
+        
+        
+        
+        
+        
+        
+        //UserDefaults.standard.integer(forKey: <#T##String#>)
+        
+        UserDefaults.standard.set(segvalueToSave, forKey: "segid")
+        UserDefaults.standard.synchronize()
+
+    }
+    @IBAction func settipper(_ sender: AnyObject) {
+        
+        let segvalueToSave = 2
+        //let segvalueToSave = seg[setvalueToSave.selectedSegmentIndex]
+        
+        
+        
+        
+        
+        
+        //UserDefaults.standard.integer(forKey: <#T##String#>)
+        
+        UserDefaults.standard.set(segvalueToSave, forKey: "segid")
+        UserDefaults.standard.synchronize()
+
+        
+    }
+    }
--- tippy/ViewController.swift
+++ tippy/ViewController.swift
@@ -16,10 +16,18 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var tipControl: UISegmentedControl!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        let valueToSavevc = 0
+        
+        //UserDefaults.standard.integer(forKey: <#T##String#>)
+        
+        UserDefaults.standard.set(valueToSavevc, forKey: "segid")
+        UserDefaults.standard.synchronize()
+        //_ = UserDefaults.standard.integer(forKey: "segid")
+            }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -37,8 +45,13 @@ class ViewController: UIViewController {
         
         let tipPercentages = [0.18, 0.2, 0.25]
         
+        
+        let tipindex = UserDefaults.standard.integer(forKey: "segid")
+        
+                
         let bill = Double(billField.text!) ?? 0
-        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        //let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let tip = bill * tipPercentages[tipindex]
         let total = bill + tip
         
         gratuityLabel.text = String(format: "$%.2f", tip)
